f66f35dcc2eeb0d6b51163de0b5d85cb1757250d
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2024-11-19 00:30:57 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel='icon' href='/Microbird.png' />
+      <link rel='icon' href='https://www.imgtr.net/ib/CjAUT5uoOWluiz8_1731955214.png' />
       </Head>
       <body className="antialiased">
         <Main />
